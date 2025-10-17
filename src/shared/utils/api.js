2eeabfdf6fcd3c69327d9936ba884291d9d6bb5a
edu-yoshida <eduardo41543@fiec.edu.br>
2025-10-16 22:05:17 -0300

    Implementação de token

--- src/shared/utils/api.js
+++ src/shared/utils/api.js
@@ -1,7 +1,14 @@
+// src/shared/utils/api.js
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL
-})
+  baseURL: "http://localhost:5000/v1/api", // ou apenas "http://localhost:5000" dependendo das rotas do backend
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
 export default api;
