ca38826a7aa2ce91083ae697ece064c584738cfb
edu-yoshida <eduardo41543@fiec.edu.br>
2025-10-23 22:09:01 -0300

    Adição de rota pública

---
+++ src/shared/components/PublicRoute.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {useAuth} from '../context/AuthContext'
+import { Navigate } from 'react-router-dom';
+import useAuthStore from '../store/auth-store';
+
+const PublicRoute = ({children}) => {
+  //const {isAuthenticated } = useAuth();
+  const {user} = useAuthStore();
+  return (
+    user == null ? children : <Navigate to={"/"} replace />
+  )
+}
+
+export default PublicRoute
