af1f88971926250a4862b2d88d0bcac19103f97f
edu-yoshida <eduardo41543@fiec.edu.br>
2025-10-23 22:08:38 -0300

    Sidebar atualizada

--- src/shared/components/Sidebar.js
+++ src/shared/components/Sidebar.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { FaUser, FaBox, FaCogs, FaCheckCircle, FaBars, FaArrowLeft } from "react-icons/fa";
+import { FaUser, FaBox, FaCogs, FaCheckCircle} from "react-icons/fa";
 import { Link } from "react-router-dom";
 import LogoGastroFlow from "../../assets/LogoGastroFlow.png";
 
 const SidebarSection = ({ icon: Icon, title, links }) => {
   return (
     <div>
-      <div className="flex items-center gap-2 px-4 py-2 text-gray-300 uppercase text-xs font-bold">
-        {Icon && <Icon className="text-gray-400" />}
+      <div className="flex items-center gap-2 px-4 py-2 text-gray-800 uppercase text-xs font-bold">
+        {Icon && <Icon className="text-gray-800" />}
         {title}
       </div>
 
@@ -16,14 +16,14 @@ const SidebarSection = ({ icon: Icon, title, links }) => {
           <Link
             to={item.path}
             key={index}
-            className="block px-4 py-2 text-sm text-black hover:bg-orange-100 hover:bg-opacity-60 rounded-md mr-10"
+            className="block px-4 py-2 text-sm text-gray-550 hover:bg-orange-200 hover:bg-opacity-60 rounded-md mr-10"
           >
             {item.label}
           </Link>
         ))}
       </div>
     </div>
-  );
+  ); 
 };
 
 const Sidebar = () => {
@@ -31,36 +31,16 @@ const Sidebar = () => {
 
   return (
     <>
-      {/* Botão Hamburguer (quando sidebar fechada) */}
-      {!isOpen && (
-        <button
-          onClick={() => setIsOpen(true)}
-          className="fixed top-4 left-4 z-50 p-3 bg-[#000000] text-black-800 rounded-md hover:bg-gray-700"
-        >
-          <FaBars size={20} />
-        </button>
-      )}
-
       {/* Sidebar */}
       {isOpen && (
         <div className="w-64 bg-[#ffffff] text-black-800 flex flex-col h-screen fixed top-0 left-0 z-40">
-          {/* Botão de seta no topo */}
-          <div className="flex justify-end p-4">
-            <button
-              onClick={() => setIsOpen(false)}
-              className="text-white hover:text-gray-300"
-            >
-              <FaArrowLeft size={20} />
-            </button>
-          </div>
-
           {/* Logo */}
           <div className="flex justify-center items-center py-6">
             <Link to="/produtos">
               <img
                 src={LogoGastroFlow}
                 alt="Logo"
-                className="w-32 cursor-pointer hover:opacity-80 transition bg-white rounded-lg border-5"
+                className="w-50 cursor-pointer hover:opacity-80 transition bg-white rounded-lg border-5"
               />
             </Link>
           </div>
@@ -70,7 +50,7 @@ const Sidebar = () => {
             icon={FaUser}
             title="Usuários"
             links={[
-              { label: "Cadastro de Usuário", path: "/usuarios" },
+              { label: "Cadastro de Usuário", path: "/CadastroUsuario" },
               { label: "Verificação de Acessos", path: "/usuarios/acessver" },
             ]}
           />
@@ -106,14 +86,14 @@ const Sidebar = () => {
           <div className="mt-auto">
             <Link
               to="/configuracoes"
-              className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700"
+              className="flex items-center gap-2 px-4 py-2 hover:bg-gray-300"
             >
               <FaCogs />
               <span>Configurações</span>
             </Link>
             <Link
               to="/relatorios"
-              className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700"
+              className="flex items-center gap-2 px-4 py-2 hover:bg-gray-300"
             >
               <FaCheckCircle />
               <span>Relatórios</span>
