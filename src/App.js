13423b40b992707a12c296750f85e2931d074658
edu-yoshida <eduardo41543@fiec.edu.br>
2025-11-07 22:49:34 -0300

    Correção de import

--- src/App.js
+++ src/App.js
@@ -1,58 +1,58 @@
 
-  import { useState } from "react";
-  import { HashRouter as Router, Routes, Route } from "react-router-dom";
-  import './App.css';
+import { useState } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
 
-  import CadastroUsuario from "./features/home/components/CadastroUsuario";
-  import CadastroDeProdutos from "./features/home/components/CadastroDeProdutos";
-  import CadastroDeReceita from "./features/recipe/CadastroDeReceita";
-  import AulasReceitas from "./features/recipe/AulasReceitas";
-  import CadastroCompra from "./features/supplies/Compra";
-  import GerenciarProdutos from "./features/storage/GerenciarProdutos";
-  import Login from './features/home/components/Login';
-  import SplashScreen from './features/home/components/SplashScreen';
-  import Fornecedor from "./features/supplies/Fornecedor";
-  import { AuthProvider } from "./shared/context/AuthContext";
-  import React from 'react';
-  import { Toaster } from 'react-hot-toast';
-  import { requestForToken, onMessageListener } from './firebase';
-  import useAuthStore from './shared/store/auth-store';
-  import PublicRoute from "./shared/components/PublicRoute";
-  import PrivateRoute from "./shared/components/PrivateRoute";
+import CadastroUsuario from "./features/home/components/CadastroUsuario";
+import CadastroDeProdutos from "./features/home/components/CadastroDeProdutos";
+import CadastroDeReceita from "./features/recipe/CadastroDeReceita";
+import AulasReceitas from "./features/recipe/AulasReceitas";
+import CadastroCompra from "./features/supplies/Compra";
+import Login from './features/home/components/Login';
+import SplashScreen from './features/home/components/SplashScreen';
+import Fornecedor from "./features/supplies/Fornecedor";
+import { AuthProvider } from "./shared/context/AuthContext";
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import { requestForToken, onMessageListener } from './firebase';
+import useAuthStore from './shared/store/auth-store';
+import PublicRoute from "./shared/components/PublicRoute";
+import PrivateRoute from "./shared/components/PrivateRoute";
+import VerificarProdutos from "./features/storage/VerificarProdutos";
 
-  function App() {
-    const [showSplash, setShowSplash] = useState(true);
+function App() {
+  const [showSplash, setShowSplash] = useState(true);
 
-    const [token, setToken] = React.useState(null);
-    const { setFcmToken } = useAuthStore()
+  const [token, setToken] = React.useState(null);
+  const { setFcmToken } = useAuthStore()
 
-    // 1. Obtém o Token na montagem do componente
-    React.useEffect(() => {
-      requestForToken()
-        .then((currentToken) => {
-          setToken(currentToken);
-          setFcmToken(currentToken)
-        })
-        .catch((err) => {
-          console.error("Erro ao solicitar token:", err);
-        });
-    }, []);
-
-    // 2. Configura o Listener para mensagens em Foreground
-    React.useEffect(() => {
-      // Registra o listener e obtém a função de limpeza (unsubscribe)
-      const unsubscribe = onMessageListener().then(() => {
-        // O then é chamado quando uma mensagem chega, mas o listener continua ativo
+  // 1. Obtém o Token na montagem do componente
+  React.useEffect(() => {
+    requestForToken()
+      .then((currentToken) => {
+        setToken(currentToken);
+        setFcmToken(currentToken)
+      })
+      .catch((err) => {
+        console.error("Erro ao solicitar token:", err);
       });
+  }, []);
 
-      // Retorna a função de limpeza para que o listener seja removido ao desmontar o componente
-      return () => {
-        // Se necessário, implemente o unsubscribe do listener aqui.
-        // Para onMessage(), o listener é geralmente ativo enquanto o app está montado.
-      };
-    }, []);
+  // 2. Configura o Listener para mensagens em Foreground
+  React.useEffect(() => {
+    // Registra o listener e obtém a função de limpeza (unsubscribe)
+    const unsubscribe = onMessageListener().then(() => {
+      // O then é chamado quando uma mensagem chega, mas o listener continua ativo
+    });
 
-    return (
+    // Retorna a função de limpeza para que o listener seja removido ao desmontar o componente
+    return () => {
+      // Se necessário, implemente o unsubscribe do listener aqui.
+      // Para onMessage(), o listener é geralmente ativo enquanto o app está montado.
+    };
+  }, []);
+
+  return (
     <AuthProvider>
       <div>
         {showSplash ? (
@@ -62,11 +62,11 @@
             <Routes>
               <Route path="/" element={<Login />} />
 
-              <Route 
-                path="/CadastroUsuario" 
+              <Route
+                path="/CadastroUsuario"
                 element={
                   // <PrivateRoute>
-                    <CadastroUsuario />
+                  <CadastroUsuario />
                   // </PrivateRoute>
                 }
               />
@@ -75,14 +75,14 @@
                 element={
                   <PrivateRoute>
                     <CadastroDeProdutos />
-                  </PrivateRoute> 
+                  </PrivateRoute>
                 }
               />
               <Route
                 path="/produtos/add"
                 element={
                   <PrivateRoute>
-                    <GerenciarProdutos />
+                    <VerificarProdutos />
                   </PrivateRoute>
                 }
               />
@@ -125,6 +125,6 @@
       </div>
     </AuthProvider>
   );
-  }
+}
 
-  export default App;
+export default App;
